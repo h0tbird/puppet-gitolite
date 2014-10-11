db76ed32220a512c80d4d985d30e1fc4addf9faf
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-11 17:05:05 +0200

    Fix mode declaration

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,9 +2,10 @@ class gitolite::config inherits gitolite {
 
   file { "/home/${git_user}/.gitolite.rc":
     ensure  => present,
+    content => template("${module_name}/gitolite.rc.erb"),
     owner   => $git_user,
     group   => $git_user,
-    mode    => template("${module_name}/gitolite.rc.erb"),
+    mode    => '0640',
     require => Exec['gitolite_setup'],
   }
 }
