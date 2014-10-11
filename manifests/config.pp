99344016848f7f1db6c0052466ab58845413a2c7
Marc Villacorta <marc.villacorta@gmail.com>
2014-10-11 16:55:52 +0200

    Manage the .gitolite.rc file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,10 @@
 class gitolite::config inherits gitolite {
 
+  file { "/home/${git_user}/.gitolite.rc":
+    ensure  => present,
+    owner   => $git_user,
+    group   => $git_user,
+    mode    => template("${module_name}/gitolite.rc.erb"),
+    require => Exec['gitolite_setup'],
+  }
 }
